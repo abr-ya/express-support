fe475ce170284f36cb4cf474c06c2007a1e32bd9
abr-ya <gamer05@list.ru>
2022-10-16 10:24:52 +0300

    refactor: Add ESLint and Prettier

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+const dotenv = require("dotenv").config();
+
 const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support Desk API' })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Support Desk API",
+  });
+});
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
