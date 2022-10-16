13f04857524cc8877d00cfe21e3e1e3d7077f7de
abr-ya <gamer05@list.ru>
2022-10-16 13:10:46 +0300

    feat: Add errorMiddleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // eslint-disable-next-line no-unused-vars
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Support Desk API!" });
 });
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Middlewares
+app.use(errorHandler);
 
 // Start
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ src/middleware/errorMiddleware.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, _req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = { errorHandler };
