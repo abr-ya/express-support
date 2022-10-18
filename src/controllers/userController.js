925cd66da59a24f341425f94e48aae7e50948a7d
abr-ya <gamer05@list.ru>
2022-10-18 21:18:42 +0300

    feat: Add Auth Middleware

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -80,7 +80,20 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
+});
+
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
