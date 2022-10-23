fddc89187ba1c94054b9f8ceb27be7057133dd08
abr-ya <gamer05@list.ru>
2022-10-23 16:38:14 +0300

    feature: Add CORS

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 /* eslint-disable no-unused-vars */
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+const FRONT_LIST = [process.env.FRONT1, process.env.FRONT2, process.env.FRONT3]
 
 // Connect to database
 connectDB();
 
 const app = express();
+app.use(cors({ origin: FRONT_LIST, credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
